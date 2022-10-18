2fdd125ef345d8ce94635e86cf86652355ab633e
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-10-18 16:29:13 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,31 @@
 class Person
+    def job=(job)
+        @job = job
+    end
 
+    def job
+        @job
+    end
+
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
 end
+
+
+
+
+
+# def name=(name)
+#     @name = name
+#   end
+
+#   # getter method
+#   def name
+#     @name
+#   end
